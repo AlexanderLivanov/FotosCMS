1420dde9ff8852313ed093ad61046f2aa95154bc
Alexander <sashalivanov2007@gmail.com>
2023-02-01 22:38:26 +0700

    some changes

--- sec/content-1.php
+++ sec/content-1.php
@@ -2,11 +2,12 @@
 
 echo '<h3>Текущие настройки:</h3>
 <p>Название сайта:'.$site_name.'</p>
-<p>Имя владельца:'.$author.'</p>'
+<p>Имя владельца:'.$author.'</p>';
+
 ?>
 
-<form action="update-settings.php" method="post" enctype="multipart/form-data">
-    Название сайта: <input type="text" placeholder="Мой сайт"><br>
-    Имя владельца: <input type="text" placeholder="Админ"><br>
+<form action="../../sec/update-settings.php" method="post" enctype="multipart/form-data">
+    Название сайта: <input type="text" placeholder="Мой сайт" name="sitenameinput"><br>
+    Имя владельца: <input type="text" placeholder="Админ" name="adminnameinput"><br>
     <input type="submit" value="Сохранить">
 </form>
---
+++ sec/update-settings.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once('strings.php');
+if(!empty($_POST)){
+    $site_name = $_POST['sitenameinput'];
+};
+
+?>
