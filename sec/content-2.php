f975647d03af9c606308e7227b6bdef22cc8ee51
Alexander <sashalivanov2007@gmail.com>
2023-02-20 13:10:40 +0300

    Я передумал

--- sec/content-2.php
+++ sec/content-2.php
@@ -3,11 +3,3 @@
     <input type="file" name="file[]" multiple>
     <input type="submit" value="Отправить">
 </form>
-
-<!-- <form action="save-copy.php" method="post" enctype="multipart/form-data">
-    <p>
-    <h3>Копирайт:</h3>
-    <input type="text" name="copyright-name" <?php echo ('value=' . $_SESSION['copyrigth-name'] . ''); ?>>
-    <input type="submit" value="Обновить копирайт">
-    </p>
-</form> -->
